7a6ab62834fcc259dfc25b93afbe4da206eead15
prohil <antonnov-420@yandex.ru>
2020-10-19 09:07:48 +0300

    update script.py

--- script.py
+++ script.py
@@ -16,31 +16,4 @@ class GnewsSpider(scrapy.Spider):
                 'href': response.urljoin(href),
                 'datetime': datetime,
             }
-            #data = json.loads(response.body)
-            #print(data)
 
-        # if data['has_next']:
-        #    next_page = data['page'] + 1
-        #    yield scrapy.Request(self.quotes_base_url % next_page)
-
-        # for car_div in response.css('.ListingItem-module__main'):
-
-        # link = car_div.css('a.ListingItemTitle-module__link')
-        # title = link.css('::text').get()
-        # href = link.css('::attr(href)').get()
-
-        # Some prices in span tag, some in div tag
-        # raw_price = car_div.css('.ListingItemPrice-module__content::text').get()
-        # if not raw_price:
-        #    raw_price = car_div.css('.ListingItemPrice-module__content').css('span::text').get()
-
-        ##clear unexpected symbols
-        # price = raw_price and clean_price(raw_price) or None
-        # img_urls = car_div.css('.Brazzers__image::attr(data-src)').getall()
-        # 'title': title,
-        # 'href': href,
-        # 'price': price,
-        # 'imgs': [response.urljoin(img) for img in img_urls],
-
-#       for next_page in response.css('a.next-posts-link'):
-#           yield response.follow(next_page, self.parse)
